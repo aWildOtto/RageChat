a5b24a176703f0799517de610208e4c38d2f2aff
ohu <otto_hu@sfu.ca>
2020-02-22 14:53:28 -0800

    photo stuff

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ def sessions():
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
+@socketio.on('message')
+def handle_message_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
+@socketio.on('photo')
+def handle_photo_event(photo, methods=['GET', 'POST']):
+    print("received photo")
+		# photo is based64 blob, decode and do emotion detection
+		# do emotion detection
+    socketio.emit('photo result', "angry")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
