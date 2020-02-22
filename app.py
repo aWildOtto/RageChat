5a04a7c2755b2b8d705b6e141e80af84ec3d5512
David <wdsong@sfu.ca>
2020-02-22 14:53:31 -0800

    room stuff in py file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+alldata = {}
 @app.route('/')
 def sessions():
     return render_template('session.html')
@@ -16,6 +17,20 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    socketio.send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    socketio.send(username + ' has left the room.', room=room)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
